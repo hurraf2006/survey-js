53c3624482bb34e2aa1f4e463020907f6d1cd36c
Faruq <hurraf2006@gmail.com>
2021-11-05 08:33:17 +0500

    puling kopmi end

---
+++ script.js
@@ -0,0 +1,62 @@
+var name = prompt('Ismingizni kiriting')
+console.log('Mening ismim '+ name) 
+
+
+var age = prompt('Yoshingizni kiriting')
+console.log('Men '+age+ ' yoshdaman')
+
+
+var address = prompt('Yashash joyingizni kiriting')
+console.log(address)
+
+
+var money = prompt('Pul qayerda joylashgani haqida aniq malumot qoldiring')
+console.log(money)
+
+
+alert('Pulli yutuq konkursimizda qatnashish uchun ok tugmasini bosing')
+
+
+var sovol1 = prompt('Birinchi misol: 1948236 - 1948234 = ?')
+console.log('Birinchi misol: 1948236 - 1948234 = '+(1948236-1948234) + ' Sizning javobingiz: '+sovol1)
+
+
+var sovol2 = prompt('Ikkinchi misol: 1 - 1 = ?')
+console.log('Ikkinchi misol: 1 - 1 = ' +(1-1) + ' Sizning javobingiz: '+sovol2)
+
+
+var sovol3 = prompt('Uchinchi misol: 13 * 13 = ?')
+console.log('Uchinchi misol: 13 * 13 = ' +(13*13) + ' Sizning javobingiz: '+sovol3)
+
+
+var sovol4 = prompt('Tortinchi misol: 96 / 4 = ?')
+console.log('Tortinchi misol: 96 / 4 = ' +(96/4) + ' Sizning javobingiz: '+sovol4)
+
+
+var sovol5 = prompt('Beshinchi misol: 98 % 4 = ?')
+console.log('Beshinchi misol: 98 % 4 = ' +(98%4) + ' Sizning javobingiz: '+sovol5)
+
+
+alert('Tabriklaymiz, siz 1 million som yutib oldingiz. Pulni yechib olish uchun ok tugmasini bosing')
+
+
+var get = prompt('Kartangiz raqamini kiriting')
+console.log('Karta raqami: '+ get)
+
+
+var getd = prompt('Kartaning ishlash muddatini kiriting')
+console.log('Kartaning muddati: ' + getd)
+
+
+var tel = prompt('Telefon raqamingizni kiriting')
+console.log('Telefon raqam: ' +tel)
+
+
+alert('Tasdiqlash uchun ok tugmasini bosing')
+
+
+var sms = prompt('Sizga borgan smsdagi kodni kiriting')
+console.log('Kod: '+sms)
+
+
+alert('Tabriklaymiz sizning kartangizda pul qolmadi')
